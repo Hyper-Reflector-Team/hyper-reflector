4d55702b069fa990c91b07ed410361347a8d1eba
Dustin Walker! <dustinwalkerart@gmail.com>
2025-04-28 10:26:39 +0900

    fixes bug where match details opened automatically

--- src/front-end/pages/PlayerProfilePage.tsx
+++ src/front-end/pages/PlayerProfilePage.tsx
@@ -81,6 +81,7 @@ export default function PlayerProfilePage() {
         setRecentMatches([])
         setEditedUserName(undefined)
         setIsEditName(false)
+        setSelectedMatchDetails(undefined)
     }
 
     const handleSetRecentMatches = (matchData) => {
