7479f7735c5188bdc3c471d03e52ee4d723f9853
Dustin Walker! <dustinwalkerart@gmail.com>
2025-04-29 10:22:31 +0900

    versioning updates

--- src/front-end/layout/Layout.tsx
+++ src/front-end/layout/Layout.tsx
@@ -197,7 +197,7 @@ export default function Layout({ children }) {
                     https://discord.gg/T77dSXG7Re
                 </Text>
                 <Text textStyle="xs" color={theme.colors.main.action}>
-                    Hyper Reflector version 0.2.3a 2025
+                    Hyper Reflector version 0.3.0a 2025
                 </Text>
             </Box>
         </Stack>
