c24710f86bf81b05c449fc2ab029f14da0ff9191
Dustin Walker! <dustinwalkerart@gmail.com>
2025-07-17 17:16:03 +0900

    checking reversion

--- src/loadFbNeo.ts
+++ src/loadFbNeo.ts
@@ -86,8 +86,8 @@ export function startPlayingOnline({
     if (isTraining) {
         luaPath = config.emulator.trainingLuaPath
     }
-    // const directCommand = `${fightcadeCmd(config)} quark:direct,sfiii3nr1,${localPort},${remoteIp},${remotePort},${player},${delay},0 --lua ${luaPath}`
-    const directCommand = `${fightcadeCmd(config)} --rom sfiii3nr1 direct --player 2 -n TestGuy -l ${"127.0.0.1" + ':' + localPort} -r ${remoteIp + ':' + remotePort}`
+    const directCommand = `${fightcadeCmd(config)} quark:direct,sfiii3nr1,${localPort},${remoteIp},${remotePort},${player},${delay},0 --lua ${luaPath}`
+    // const directCommand = `${fightcadeCmd(config)} --rom sfiii3nr1 direct --player 2 -n TestGuy -l ${"127.0.0.1" + ':' + localPort} -r ${remoteIp + ':' + remotePort}`
     // const directCommand = `${fightcadeCmd(config)} --rom sfiii3nr1 direct -n TestGuy -l ${"127.0.0.1" + ':' + localPort} -r ${remoteIp + ':' + remotePort} -d 1 -p ${2}`
     switch (process.platform) {
         case 'darwin':
@@ -106,9 +106,8 @@ export function startSoloMode({
     config: Config
     callBack: (isOnOpen?: boolean) => any
 }) {
-    // const directCommand = `${fightcadeCmd(config)} --rom sfiii3nr1 direct -n Test -l 127.0.0.1:7000 -r 127.0.0.1:7000 -d 0 --player 1 `
-    // const directCommand = `${fightcadeCmd(config)} -game sfiii3nr1 ${config.emulator.trainingLuaPath}`
-    const directCommand = `${fightcadeCmd(config)} --rom sfiii3nr1 --lua ${config.emulator.trainingLuaPath}`
+    const directCommand = `${fightcadeCmd(config)} -game sfiii3nr1 ${config.emulator.trainingLuaPath}`
+    // const directCommand = `${fightcadeCmd(config)} --rom sfiii3nr1 --lua ${config.emulator.trainingLuaPath}`
     return launchGGPOSpawn(directCommand, callBack)
 }
 
