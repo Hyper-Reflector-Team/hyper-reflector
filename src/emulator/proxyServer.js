0c5f5c16792ec1778a982ee633c7169da809f574
Dustin Walker! <dustinwalkerart@gmail.com>
2025-08-09 16:53:06 +0900

    begin setup for go proxy

--- src/emulator/proxyServer.js
+++ src/emulator/proxyServer.js
@@ -1,4 +1,7 @@
+const { spawn } = require('child_process')
 const dgram = require('dgram')
+import { app } from 'electron'
+import path from 'path'
 import keys from '../private/keys'
 import { startPlayingOnline } from '../loadFbNeo'
 
@@ -13,6 +16,25 @@ let opponentEndpoint = null // This is the address and port we use to reach our 
 let userUID = null // we'll send this in from main.ts
 let userName = null
 let opponentUID = null
+
+// Golang proxy setup and path
+// let proxyProc = null // used to spawn the goProxy
+
+// function resolveGoProxyPath() {
+//     const exe = process.platform === 'win32' ? 'goProxy.exe' : 'goProxy'
+//     const isProd = app.isPackaged
+
+//     if (isProd) {
+//         const p = path.join(process.resourcesPath, 'emulator', exe)
+//         return p
+//     }
+
+//     const projectRoot = path.resolve(__dirname, '..', '..')
+//     const p = path.join(projectRoot, 'src', 'emulator', exe)
+//     return p
+// }
+
+// const goProxyPath = resolveGoProxyPath()
 
 export default async function runProxyServer(
     data,
@@ -101,6 +123,46 @@ export default async function runProxyServer(
         }
 
         sendMessageToS(false)
+
+        // New Golang proxy code
+        // const args = [
+        //     `-serverHost=${keys.COTURN_IP}`,
+        //     `-serverPort=${keys.PUNCH_PORT}`,
+        //     `-uid=${userUID}`,
+        //     `-peerUID=${proxyStartData.opponentUID}`,
+        //     `-emuIn=7004`,
+        //     `-emuOut=7005`,
+        // ]
+
+        // try {
+        //     proxyProc = spawn(goProxyPath, args, {
+        //         cwd: process.cwd(),
+        //     })
+
+        //     proxyProc.stdout.on('data', (data) => {
+        //         console.log(`prxy data: ${data.toString()}`)
+        //     })
+
+        //     proxyProc.stderr.on('data', (data) => {
+        //         console.error(`prxy error: ${data.toString()}`)
+        //         return 'prxy error'
+        //     })
+
+        //     // Listen for process exit
+        //     proxyProc.on('exit', (code, signal) => {
+        //         if (code !== null) {
+        //             console.log(`prxy exit ${code}`)
+        //         } else {
+        //             console.log(`prxy exit signal ${signal}`)
+        //         }
+        //     })
+
+        //     proxyProc.on('error', (error) => {
+        //         console.error(`prxy error: ${error.message}`)
+        //     })
+        // } catch (error) {
+        //     console.error(`Launch error prxy: ${error}`)
+        // }
     }
 }
 
