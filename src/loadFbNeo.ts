8c328adec6857247e90dbe1a2aac8aaed0f136f8
Dustin Walker! <dustinwalkerart@gmail.com>
2025-08-17 14:03:29 +0900

    fixes some match duplication issues

--- src/loadFbNeo.ts
+++ src/loadFbNeo.ts
@@ -125,14 +125,14 @@ export function startSoloMode({
     let directCommand
 
     // uncomment to send use the match data sender
-    // let luaPath = config.emulator.luaPath
-    // directCommand = `${fbNeoCommand(config)} --rom sfiii3nr1 --lua ${luaPath}`
+    let luaPath = config.emulator.luaPath
+    directCommand = `${fbNeoCommand(config)} --rom sfiii3nr1 --lua ${luaPath}`
 
-    if (slicedPathEnd === 'fs-fbneo.exe') {
-        directCommand = `${fbNeoCommand(config)} --rom sfiii3nr1 --lua ${config.emulator.trainingLuaPath}` // fs fbneo
-    } else if (slicedPathEnd === 'fcadefbneo.exe') {
-        directCommand = `${fbNeoCommand(config)} -game sfiii3nr1 ${config.emulator.trainingLuaPath}`
-    }
+    // if (slicedPathEnd === 'fs-fbneo.exe') {
+    //     directCommand = `${fbNeoCommand(config)} --rom sfiii3nr1 --lua ${config.emulator.trainingLuaPath}` // fs fbneo
+    // } else if (slicedPathEnd === 'fcadefbneo.exe') {
+    //     directCommand = `${fbNeoCommand(config)} -game sfiii3nr1 ${config.emulator.trainingLuaPath}`
+    // }
 
     return launchGGPOSpawn(directCommand, callBack)
 }
